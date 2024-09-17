f5c1215f1de7a4914612f3c7fca1c27213100ba1
rahulemathi <erahul096@gmail.com>
2024-09-17 15:23:20 +0530

    fixed notification close

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -35,7 +35,7 @@
               <p class="text-sm font-medium text-gray-900">{{ Session::get('success') }}</p>
             </div>
             <div class="ml-4 flex flex-shrink-0">
-              <button type="button" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onclick="close()">
+              <button type="button" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onclick="document.getElementById('notification').style.display='none'">
                 <span class="sr-only">Close</span>
                 <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
@@ -51,30 +51,17 @@
     {{ $slot }}
 
     <script>
-      // document.addEventListener('DOMContentLoaded', function () {
+      document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.done-btn').forEach(function (button) {
         button.addEventListener('click', function () {
             // Find the sibling <p> element and toggle the line-through style
             const todoText = this.parentNode.querySelector('.todo-text');
             todoText.style.textDecoration = todoText.style.textDecoration === 'line-through' ? 'none' : 'line-through';
-            
-
-            if(todoText.style.textDecoration === 'line-through'){
-              const buttonText = this.parentNode.querySelector('.done-btn');
+            const buttonText = this.parentNode.querySelector('.done-btn');
             buttonText.innerHTML = 'Undone'
-            }else{
-              const buttonText = this.parentNode.querySelector('.done-btn');
-            buttonText.innerHTML = 'Done'
-            }
         });
     });
-
-    function close(){
-  document.getElementById('notification').style.display = 'none';
-}
-// });
-
-
+});
     </script>
 </body>
 </html>
