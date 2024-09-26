c2534d15660e4832ef2e65dfef8f62041d595ecb
rahulemathi <erahul096@gmail.com>
2024-09-26 16:25:29 +0530

    modified the title name

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/',[TodoListController::class,'index']);
